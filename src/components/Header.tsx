4b8d9ef974ea8476d00fc0e6abac0f9d0586fdf6
alan matheus <matheusalan334@gmail.com>
2025-07-03 17:17:33 -0300

    logo add

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Menu, X, Phone, MessageCircle, Star } from 'lucide-react';
+import logo from "../assets/imgs/Design sem nome.svg"
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -49,10 +50,7 @@ const Header = () => {
           <div className="flex items-center justify-between h-16">
             {/* Logo */}
             <div className="flex items-center gap-2">
-              <div className="w-10 h-10 bg-gradient-to-br from-rose-500 to-rose-600 rounded-full flex items-center justify-center">
-                <Star className="w-5 h-5 text-white fill-current" />
-              </div>
-              <span className="text-xl font-bold text-gray-900">HarmoniaBela</span>
+             <img src={logo} alt="" />
             </div>
 
             {/* Menu Desktop */}
